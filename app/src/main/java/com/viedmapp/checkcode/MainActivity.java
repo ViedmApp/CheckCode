ba621ddbfc1f326fb24d73d291af4bb705cdb94e
moyarzun94 <38325722+moyarzun94@users.noreply.github.com>
2018-04-20 23:27:25 -0300

    Add files via upload
    Added user permissions

--- app/src/main/java/com/viedmapp/checkcode/MainActivity.java
+++ app/src/main/java/com/viedmapp/checkcode/MainActivity.java
@@ -1,7 +1,12 @@
 package com.viedmapp.checkcode;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -35,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     private boolean isVoiceActive;
     String scannedData;
     Button button;
+    private  static  final int REQUEST_CODE=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,24 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         escanerView.setAutoFocus(false);
         setContentView(R.layout.activity_main);
         showCameraLayout(R.id.camera_preview);
+        verifyPermissions();
+    }
+
+    private void verifyPermissions(){
+        Log.d("MainActivity","VerifyPermissions: asking user for permissions");
+        String[] permissions ={Manifest.permission.CAMERA};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0])== PackageManager.PERMISSION_GRANTED){
+
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,REQUEST_CODE);
+        }
+    }9
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+
+        verifyPermissions();
     }
 
     @Override
