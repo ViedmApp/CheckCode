0e124a48eb844f15469f57eaf776c89e5f0fb8df
Eduardo Vera <e_vera@outlook.com>
2018-07-29 20:37:26 -0400

    Added AlertDialog to wait until the sheet is created.

--- app/src/main/java/com/viedmapp/checkcode/AsyncTasks/DataHandshake.java
+++ app/src/main/java/com/viedmapp/checkcode/AsyncTasks/DataHandshake.java
@@ -1,8 +1,10 @@
 package com.viedmapp.checkcode.AsyncTasks;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.AsyncTask;
 
-import com.google.api.client.http.HttpResponse;
+import com.viedmapp.checkcode.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -19,30 +22,22 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class DataHandshake extends AsyncTask<String,Void,String> {
     public AsyncResponse delegate = null;
+    private WeakReference<Activity> activityWeakReference;
+    private AlertDialog alertDialog;
+
+    public DataHandshake(Activity activityWeakReference) {
+        this.activityWeakReference = new WeakReference<>(activityWeakReference);
+    }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-
-        //Parsing
-        try{
-            JSONArray jsonArray = new JSONArray(result);
-            JSONObject jsonObject = (JSONObject) jsonArray.get(0);
-
-            ArrayList<String> jData = new ArrayList<>();
-            jData.add(jsonObject.getString("Email"));
-            jData.add(jsonObject.getString("Url"));
-            jData.add(jsonObject.getString("id"));
-
-            delegate.processFinish(jData);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        AlertDialog.Builder builder=new AlertDialog.Builder(activityWeakReference.get());
+        builder.setTitle(R.string.dialog_creating_datasheet);
+        builder.setView(R.layout.dialog_wait);
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
@@ -80,19 +75,40 @@ public class DataHandshake extends AsyncTask<String,Void,String> {
             String line = "";
             StringBuilder data = new StringBuilder();
 
-            while(line!=null){
+            while (line != null) {
                 line = bufferedReader.readLine();
                 data.append(line).append("\n");
             }
 
             inputStream.close();
 
-            return data.toString().substring(data.indexOf("["),data.lastIndexOf("]")+1);
+            return data.toString().substring(data.indexOf("["), data.lastIndexOf("]") + 1);
 
         } catch (IOException e) {
             e.printStackTrace();
             return "Error";
         }
 
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        //Parsing
+        try{
+            JSONArray jsonArray = new JSONArray(result);
+            JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+
+            ArrayList<String> jData = new ArrayList<>();
+            jData.add(jsonObject.getString("Email"));
+            jData.add(jsonObject.getString("Url"));
+            jData.add(jsonObject.getString("id"));
+
+            alertDialog.dismiss();
+            delegate.processFinish(jData);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
